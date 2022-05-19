ea7251e7af62c21a9ff79dec103a60c4fa579b8e
one-chance <quwieo@ajou.ac.kr>
2022-05-19 10:50:41 +0900

    feat: set absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, `./src/`),
+      '@': path.resolve(__dirname, `./src`),
+      '@apis': path.resolve(`./src/`, `apis`),
+      '@assets': path.resolve(`./src/`, `assets`),
+      '@components': path.resolve(`./src/`, `components`),
+      '@pages': path.resolve(`./src/`, `pages`),
+      '@styles': path.resolve(`./src/`, `styles`),
+      '@utils': path.resolve(`./src/`, `utils`),
     },
   },
 });
