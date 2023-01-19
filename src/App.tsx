c3bcab592464d7049a479ca9517fa1673f2066f4
one-chance <quwieo@ajou.ac.kr>
2023-01-19 15:25:46 +0900

    remove: unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-/* eslint-disable no-underscore-dangle */
-import { Anchor } from '@components/Common';
-import { useState, Fragment } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Footer, Header, Main } from '@components/Layout';
+import React from 'react';
 
-import './App.css';
-
-const BASIC = import.meta.globEager(`/src/pages/404.tsx`);
-const COMPONENTS: Record<string, { [key: string]: any }> =
-  import.meta.globEager(`/src/pages/**/[a-z[]*.tsx`);
-
-const basics = Object.keys(BASIC).reduce((basic, file) => {
-  const key = file.replace(/\/src\/pages\/|\.tsx$/g, ``);
-  return { [key]: BASIC[file].default };
-}, {});
-
-const components = Object.keys(COMPONENTS).map(component => {
-  const path = component
-    .replace(/\/src\/pages|index|\.tsx$/g, ``)
-    .replace(/\[\.{3}.+\]/, `*`)
-    .replace(/\[(.+)\]/, `:$1`);
-
-  return { path, component: COMPONENTS[component].default };
-});
-
-export default () => {
-  const a = 1;
-
-  return (
-    <Routes>
-      {components.map(({ path, component: Component = Fragment }) => (
-        <Route key={path} element={Component()} path={path} />
-      ))}
-    </Routes>
-  );
-};
+export default () => (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+);
--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { FlexView, Text } from '@components/Common';
-
-export default () => (
-  <FlexView>
-    <Text>페이지를 찾을 수 없습니다.</Text>
-  </FlexView>
-);
